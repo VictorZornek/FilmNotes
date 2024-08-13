d7c7e1cfe9c0f9148f70f71d68d8a2887cbdc153
Victor Zornek <vzornek@gmail.com>
2024-08-13 18:21:53 -0300

    add metodos de create e update no UsersController.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,6 +7,7 @@ const usersRouter = Router();
 const usersController = new UsersController();
 
 usersRouter.post("/", usersController.create);
+usersRouter.put("/:id", usersController.update);
 
 
 module.exports = usersRouter;
