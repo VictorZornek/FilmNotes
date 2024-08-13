c25f4187daa53dbd2828d292738a05efe3e73c7a
Victor Zornek <vzornek@gmail.com>
2024-08-13 12:26:31 -0300

    add pasta routes, e criação da primeira route do projeto (users)

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const express = require("express");  //importa o módulo do express
 
+const routes = require('./routes')
+
 const app = express();  // inicializa o app com express
+app.use(express.json());
+
+app.use(routes);
 
 const PORT = 3000;  // define a porta para o servidor
-
-app.get('/', (request, response) => {  // definindo uma rota simples
-    response.send("Server funcionando!");
-})
-
 app.listen(PORT, () => {    // iniciando o servidor na porta definida
     console.log(`Server is running on Port ${PORT}`)
 });
