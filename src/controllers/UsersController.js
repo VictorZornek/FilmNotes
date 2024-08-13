9f2463b211fa6d59e6cf9f7d9e892c8c53e9ffb6
Victor Zornek <vzornek@gmail.com>
2024-08-13 16:47:42 -0300

    add SELECT do banco de dados no UsersController.js

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,19 @@
 const AppError = require('../utils/AppError');
 
+const sqliteConnection = require('../database/sqlite')
+
 class UsersController {
-    create(request, response) {
+    async create(request, response) {
         const { name, email, password } = request.body;
 
-        if(!name) {
-            throw new AppError("O nome é obrigatório");
+        const database = await sqliteConnection();
+        const userExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if(userExists) {
+            throw new AppError("Este email já está em uso.");
         }
 
-        response.status(201).json({ name, email, password });
+        return response.status(201).json();
     }
 }
 
