6d44063325625d04bfb26f01d7b9c811ee82d950
Victor Zornek <vzornek@gmail.com>
2024-08-16 15:09:24 -0300

    add função de update nota no projeto

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -10,6 +10,12 @@ class MovieNotesController {
             throw new AppError('Rating desse filme deve ser um número entre 1 e 5!');
         }
 
+        const checkTitle = await knex('movie_notes').where({ title });
+
+        if(checkTitle) {
+            throw new AppError('Este título já está cadastrado!')
+        }
+
         const [note_id] = await knex('movie_notes').insert({
             title,
             description,
@@ -28,6 +34,31 @@ class MovieNotesController {
         await knex('movie_tags').insert(tagsInsert);
 
         return response.json();
+    }
+
+    async update(request, response) {
+        const { user_id } = request.params;
+        const { description, rating, tags } = request.body;
+        const { id } = request.query;
+
+        const [note] = await knex('movie_notes').where({ user_id }).andWhere({ id });
+
+        if(!note){
+            throw new AppError('Nota não foi encontrada!');
+        }
+
+        if(rating < 1 || rating > 5) {
+            throw new AppError('Rating desse filme deve ser um número entre 1 e 5!');
+        }
+
+        const updatedNote = {
+            description: description ?? note.description,
+            rating: rating ?? note.rating
+        }
+
+        await knex('movie_notes').where({user_id, id}).update(updatedNote)
+
+        return response.json({ message: 'Nota atualizada com sucesso!' });
     }
 }
 
--- src/routes/movienotes.routes.js
+++ src/routes/movienotes.routes.js
@@ -7,6 +7,6 @@ const movieNotesRouter = Router();
 const movieNotesController = new MovieNotesController();
 
 movieNotesRouter.post("/:user_id", movieNotesController.create);
-
+movieNotesRouter.put("/:user_id", movieNotesController.update);
 
 module.exports = movieNotesRouter;
