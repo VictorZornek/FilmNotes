4bc4501d9fbcceb958d6c6bab831d41367d54b30
Victor Zornek <vzornek@gmail.com>
2024-10-07 15:13:14 -0300

    add sessions controller e sessions.routes.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ const { Router } = require('express');
 const usersRouter = require('./users.routes');
 const movieNotesRouter = require('./movienotes.routes');
 const movieTagsRouter = require('./movietags.routes');
+const sessionsRouter = require('./sessions.routes');
 
 const routes = Router()
 
 routes.use('/users', usersRouter)
 routes.use('/movienotes', movieNotesRouter)
 routes.use('/movietags', movieTagsRouter)
+routes.use('/sessions', sessionsRouter)
 
 
 module.exports = routes;
